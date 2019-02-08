6ec610aa1e552e7c2f25d765fd3c16172ab914cb
Roman <efim1382@gmail.com>
2019-02-08 13:36:31 +0300

    some small fixes

--- src/components/subscription-item/message/index.js
+++ src/components/subscription-item/message/index.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
 import compose from 'recompose/compose';
@@ -10,6 +10,22 @@ const cx = classnames.bind(style);
 
 class Message extends Component {
   getMessageText = () => {
+    if (this.props.message.deleted_at) {
+      return this.props.t('deleted_message');
+    }
+
+    if (this.props.message.forwarded_message_id) {
+      return this.props.t('forwarded_message');
+    }
+
+    if (this.props.message.in_reply_to_message_id) {
+      return this.props.t('replied_message');
+    }
+
+    if (this.props.message.xtag) {
+      return this.props.message.text;
+    }
+
     const isMessageHasAttachment = this.props.message.attachment;
     const isMessageAttachmentImage = isMessageHasAttachment && this.props.message.attachment.content_type.match('image/');
 
@@ -37,36 +53,18 @@ class Message extends Component {
   };
 
   render() {
-    const isMessageSimple = !this.props.message.deleted_at &&
-      !this.props.message.forwarded_message_id &&
-      !this.props.message.in_reply_to_message_id &&
-      !this.props.message.xtag;
-
+    const isAuthorNameShown = !this.props.message.deleted_at && !this.props.message.xtag;
     const author = this.props.users_list[this.props.message.user_id] || {};
     const authorNick = author.id !== this.props.currentUser.id ? author.nick || 'no nick' : this.props.t('you');
 
     return <div className={cx('message', this.props.className)}>
-      {this.props.message.deleted_at &&
-        <p className={style.text}>{this.props.t('deleted_message')}</p>
-      }
+      <p className={style.text}>
+        {isAuthorNameShown && `${authorNick}: `}
+        {this.getMessageText()}
+      </p>
 
-      {this.props.message.forwarded_message_id &&
-        <p className={style.text}>{this.props.t('forwarded_message')}</p>
-      }
-
-      {this.props.message.in_reply_to_message_id &&
-        <p className={style.text}>{this.props.t('replied_message')}</p>
-      }
-
-      {this.props.message.xtag &&
-        <p className={style.text}>{this.props.message.text}</p>
-      }
-
-      {isMessageSimple &&
-        <Fragment>
-          <p className={style.text}>{authorNick}: {this.getMessageText()}</p>
-          <span className={style.time}>{this.getLastMessageTime()}</span>
-        </Fragment>
+      {!this.props.message.deleted_at &&
+        <span className={style.time}>{this.getLastMessageTime()}</span>
       }
     </div>;
   }

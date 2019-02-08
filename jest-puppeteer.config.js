7e8984b1461b8a1c35458d847c689d15d6a20d11
fenruga <fenrugaadv@gmail.com>
2019-02-08 12:24:53 +0300

    init puppeteer jest

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  launch: {
+    headless: false, // process.env.HEADLESS !== 'false',
+  },
+};
---
+++ src/__tests__/index.spec.js
@@ -0,0 +1,77 @@
+import {resolveBy, select} from 'reselector';
+
+const resolve = resolveBy(require.resolve);
+const {SignIn: SignInSelector} = resolve('../components/sign-in_container');
+
+const signInButton = select`${SignInSelector} a[href="/sign-in/"]`;
+const signUpButton = select`${SignInSelector} a[href="/sign-up/"]`;
+
+// const url = 'https://demo-chat.universa.io';
+const url = 'http://localhost:8080/';
+
+jest.setTimeout(31000);
+
+describe('Google', () => {
+  beforeAll(async () => {
+    await page.goto('https://google.com')
+  })
+
+  it('should display "google" text on page', async () => {
+    await expect(page).toMatch('google')
+  })
+})
+
+describe('Index page', () => {
+  beforeAll(async () => {
+    await page.goto(url);
+    await page.waitForNavigation();
+  });
+
+  it('should display "Log In" button in viewport', async () => {
+    // await page.waitForSelector(signInButton, {visible: true});
+    // expect(button.isIntersectingViewport()).toEqual(true);
+    await expect(page).toMatch('Log in');
+  });
+
+  it('should display "Sign Up" button in viewport', async () => {
+    // await page.waitForSelector(signUpButton, {visible: true});
+
+    await expect(page).toMatch('Sign Up');
+  });
+
+  it('should not display "qweqwe." text on page', async () => {
+
+    await expect(page).not.toMatch('qweqwe.');
+  });
+});
+
+
+// react-puppeteer free way
+
+// import puppeteer from 'puppeteer';
+//
+// let page;
+// let browser;
+// const width = 1920;
+// const height = 1080;
+// const APP = "http://0.0.0.0:8080";
+//
+// beforeAll(async () => {
+//   browser = await puppeteer.launch({
+//     headless: false,
+//     slowMo: 80,
+//     args: [`--window-size=${width},${height}`],
+//   });
+//   page = await browser.newPage();
+//   await page.setViewport({ width, height });
+// });
+//
+// describe("qwe", () => {
+//   test("qwe", async () => {
+//     await page.goto(APP);
+//   }, 16000);
+// });
+//
+// afterAll(() => {
+//   browser.close();
+// });

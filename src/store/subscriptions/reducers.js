ffa7cc6d52c202bbb4a469e80c94b8d5908fd361
Roman <efim1382@gmail.com>
2019-04-08 14:28:30 +0300

    #42 starts with

--- src/store/subscriptions/reducers.js
+++ src/store/subscriptions/reducers.js
@@ -70,7 +70,7 @@ export default createReducer(initialState, {
     state.filtered_contacts_ids = [];
 
     if (state.filter_text) {
-      state.filtered_contacts_ids = filter(state.ids, id => getChatName(state.list[id]).toLowerCase().match(state.filter_text.toLowerCase()));
+      state.filtered_contacts_ids = filter(state.ids, id => getChatName(state.list[id]).toLowerCase().startsWith(state.filter_text.toLowerCase()));
 
       map(action.payload.messages.list, message => {
         if (!message.text || message.xtag) {

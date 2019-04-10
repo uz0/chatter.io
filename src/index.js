b4ef9cd4f4ce1cfa2bb8263455f271412806c07d
Roman <efim1382@gmail.com>
2019-04-10 17:13:05 +0300

    update version

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'normalize.css';
 import './style/index.css';
 import './i18n';
 
-window.version = 2;
+window.version = 3;
 
 const md = new mobileDetect(window.navigator.userAgent);
 const isMobile = !!md.mobile();

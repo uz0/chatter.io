5af32366a9ea38cf3e7f0ff21b5de7ff8021d5f0
hoffasha <hoffasha@gmail.com>
2019-06-20 16:41:35 +0300

    login test

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Layout } from '@/layout';
 import style from '@/layout-style.css';
+import puppeteer from 'puppeteer';
 
 describe('Layout component', () => {
   it('showing error on error added', () => {
@@ -36,4 +37,37 @@ describe('Layout component', () => {
     expect(layout.contains(<h2 className={style.title}>Request</h2>)).toEqual(true);
     expect(layout.contains(<h2 className={style.title}>Details</h2>)).toEqual(true);
   });
+});
+
+describe('Pupetteer', () => {
+  test('Login form works correctly', async () => {
+    let browser = await puppeteer.launch({
+      headless: false,
+    });
+    let page = await browser.newPage();
+
+    page.emulate({
+      viewport: {
+        width: 500,
+        height: 2400,
+      },
+      userAgent: '',
+    });
+
+    await page.goto('http://localhost:8080/sign-in');
+describe('When the user enters the correct login and password', function() {
+   it('should display an username', function(done) {
+       browser
+           .setValue('#username', 'hoffasha@gmail.com')
+           .setValue('#password', 'nastya227')
+           .click('#Log in')
+           .waitForExist('.user-panel');
+
+       expect(browser.getText('user-panel__placeholder'))
+           .toBe('Unichat');
+
+       browser.call(done);
+   });
+});
+  }, 25000);
 });

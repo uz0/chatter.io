85cc12e9df4719714115da1a1b5ac0fe455643c0
Roman <efim1382@gmail.com>
2019-09-11 10:14:39 +0300

    #180 fix dropdowns click outside

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Portal } from 'react-portal';
-import ReactDOM from 'react-dom';
 import classnames from 'classnames/bind';
 import get from 'lodash/get';
 import compose from 'recompose/compose';
 import { connect } from 'react-redux';
 import Icon from '@/components/icon';
 import { uid } from '@/helpers';
+import { ClickOutside } from 'reactjs-click-outside';
 import actions from './actions';
 import style from './style.css';
 
@@ -67,64 +67,54 @@ class Dropdown extends Component {
     });
   };
 
+  closeDropdown = () => {
+    // плагин выдает много вызовов outsideCall, ставим проверку
+    if (this.props.isShown) {
+      this.props.closeDropdown(this.props.uniqueId);
+    }
+  };
+
   onItemClick = onClick => event => {
     onClick(event);
     this.props.closeDropdown(this.props.uniqueId);
   };
 
-  componentDidMount() {
-    // eslint-disable-next-line react/no-find-dom-node
-    const buttonElement = ReactDOM.findDOMNode(this.buttonRef);
-
-    window.addEventListener('click', event => {
-      if (this.props.isShown && this.listRef && !buttonElement.contains(event.target) && !this.listRef.contains(event.target)) {
-        this.props.closeDropdown(this.props.uniqueId);
-      }
-    });
-
-    window.addEventListener('scroll', () => {
-      // в будущем проверять находится ли дропдаун внутри скролящегося элемента
-      if (this.props.isShown && this.listRef) {
-        this.props.closeDropdown(this.props.uniqueId);
-      }
-    }, true);
-  }
-
   render() {
-    return <div className={cx('dropdown', this.props.className)}>
-      {React.cloneElement(this.props.children, {
-        onClick: this.toggleDropdown,
-        ref: node => this.buttonRef = node,
-      })}
+    return <ClickOutside outsideCall={this.closeDropdown}>
+      <div className={cx('dropdown', this.props.className)}>
+        {React.cloneElement(this.props.children, {
+          onClick: this.toggleDropdown,
+        })}
 
-      {this.props.isShown &&
-        <Portal>
-          <div
-            className={style.list}
-            ref={node => this.listRef = node}
+        {this.props.isShown &&
+          <Portal>
+            <div
+              className={style.list}
+              ref={node => this.listRef = node}
 
-            style={{
-              left: this.props.left,
-              right: this.props.right,
-              top: this.props.top,
-              bottom: this.props.bottom,
-            }}
-          >
-            {this.props.items.map(item => <button
-              key={uid()}
-              onClick={this.onItemClick(item.onClick)}
-              className={cx({'_is-danger': item.isDanger})}
+              style={{
+                left: this.props.left,
+                right: this.props.right,
+                top: this.props.top,
+                bottom: this.props.bottom,
+              }}
             >
-              {item.icon &&
-                <Icon name={item.icon} />
-              }
+              {this.props.items.map(item => <button
+                key={uid()}
+                onClick={this.onItemClick(item.onClick)}
+                className={cx({'_is-danger': item.isDanger})}
+              >
+                {item.icon &&
+                  <Icon name={item.icon} />
+                }
 
-              <span>{item.text}</span>
-            </button>)}
-          </div>
-        </Portal>
-      }
-    </div>;
+                <span>{item.text}</span>
+              </button>)}
+            </div>
+          </Portal>
+        }
+      </div>
+    </ClickOutside>;
   }
 }
 

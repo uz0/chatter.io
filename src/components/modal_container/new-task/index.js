ed780c89ec472d468320fb7f3f1ebb4688ab1d01
Roman <efim1382@gmail.com>
2019-10-22 14:49:22 +0300

    attach to task

--- src/components/modal_container/new-task/index.js
+++ src/components/modal_container/new-task/index.js
@@ -1,11 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import get from 'lodash/get';
+import map from 'lodash/map';
 import compose from 'recompose/compose';
 import { connect } from 'react-redux';
 import Modal from '@/components/modal';
 import SubscriptionAvatar from '@/components/subscription-avatar';
+import Attach from '@/components/attach';
 import Button from '@/components/button';
 import Dropdown from '@/components/dropdown';
+import Loading from '@/components/loading';
+import Icon from '@/components/icon';
 import Form from '@/components/form/form';
 import Input from '@/components/form/input';
 import Textarea from '@/components/form/textarea';
@@ -31,6 +35,10 @@ class NewTask extends Component {
 
     if (this.props.description.value) {
       options['description'] = this.props.description.value;
+    }
+
+    if (this.props.uploads_id.value) {
+      options['uploads_id'] = this.props.uploads_id.value;
     }
 
     const { task } = await api.createTask({
@@ -64,6 +72,30 @@ class NewTask extends Component {
     isBlured: true,
   });
 
+  attachImages = () => {
+    const input = document.querySelector('#new-task-attach');
+
+    if (!input) {
+      return;
+    }
+
+    input.click();
+  };
+
+  onAttachmentsChange = data => {
+    const uploads_id = map(data, 'upload_id');
+
+    if (!uploads_id || uploads_id.length === 0) {
+      return;
+    }
+
+    this.props.formChange('new_task.uploads_id', {
+      value: uploads_id,
+      isTouched: true,
+      isBlured: true,
+    });
+  };
+
   componentDidMount() {
     if (this.props.details.group.type === 'private_chat') {
       const user = getOpponentUser(this.props.details);
@@ -90,6 +122,14 @@ class NewTask extends Component {
         isRequired: true,
       });
     }
+
+    this.props.formChange('new_task.uploads_id', {
+      error: '',
+      value: null,
+      isTouched: false,
+      isBlured: false,
+      isRequired: false,
+    });
   }
 
   render() {
@@ -145,17 +185,50 @@ class NewTask extends Component {
           className={style.description}
         />
 
-        <div className={style.images}>
-          <div className={style.preview} style={{'--image': 'url(/assets/default-image.jpg)'}} />
-          <div className={style.preview} style={{'--image': 'url(/assets/default-image.jpg)'}} />
-          <div className={style.preview} style={{'--image': 'url(/assets/default-image.jpg)'}} />
-        </div>
+        <Attach
+          uniqueId="new-task-attach"
+          onChange={this.onAttachmentsChange}
+        >
+          {({ images, removeAttachment }) => {
+            const isImagesExist = images.length > 0;
+
+            return <Fragment>
+              {isImagesExist &&
+                <div className={style.images}>
+                  {images.map(image => {
+                    if (!image.preview) {
+                      return;
+                    }
+
+                    const inline = { '--image': `url(${image.preview})` };
+
+                    return <div
+                      key={image.uid}
+                      style={inline}
+                      className={style.preview}
+                    >
+                      <button className={style.close} onClick={removeAttachment(image.uid)}>
+                        <Icon name="close" />
+                      </button>
+
+                      {image.isLoading &&
+                        <Fragment>
+                          <Loading className={style.file_loading} isShown />
+                        </Fragment>
+                      }
+                    </div>;
+                  })}
+                </div>
+              }
+            </Fragment>;
+          }}
+        </Attach>
 
         <p className={style.updated}>Updated by Mark Trubnikov, 10m ago</p>
 
         <div className={style.attach_actions}>
           <Button appearance="_icon-transparent" icon="dots-list" type="button" className={style.attach} />
-          <Button appearance="_icon-transparent" icon="image" type="button" className={style.attach} />
+          <Button appearance="_icon-transparent" icon="image" type="button" className={style.attach} onClick={this.attachImages} />
         </div>
       </Form>
     </Modal>;
@@ -169,6 +242,7 @@ export default compose(
       title: get(state.forms, 'new_task.title', {}),
       description: get(state.forms, 'new_task.description', {}),
       executor: get(state.forms, 'new_task.executor', {}),
+      uploads_id: get(state.forms, 'new_task.uploads_id', null),
       details: state.subscriptions.list[props.options.subscription_id],
     }),
 

ec61001445b62d0a923ad0da1d3282122c8d6575
Roman <efim1382@gmail.com>
2019-11-04 11:20:44 +0200

    delete comments

--- src/components/fake-checkbox/index.js
+++ src/components/fake-checkbox/index.js
@@ -24,27 +24,4 @@ const FakeCheckbox = ({
   </div>
 </label>;
 
-// const FakeCheckbox = ({
-//   disabled,
-//   value,
-//   label,
-//   className,
-//   onChange,
-// }) => <div className={cx('container', className)}>
-//   <label className={style.checkbox_wrapper}>
-//     <input
-//       type="checkbox"
-//       onChange={onChange}
-//       {...value ? { checked: true } : {}}
-//       {...disabled ? { disabled: true } : {}}
-//     />
-
-//     <div className={cx('switch', {'_is-checked': value})}>
-//       <div className={style.circle} />
-//     </div>
-//   </label>
-
-//   <p className={style.label}>{label}</p>
-// </div>;
-
 export default FakeCheckbox;
